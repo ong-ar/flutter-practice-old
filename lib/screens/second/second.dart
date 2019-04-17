7d348f67523ab32a35c8f78b60136b8bdfb54679
ong-ar <reclusis@gmail.com>
2019-04-17 23:12:58 +0900

    add login button, use popuntil

--- lib/screens/second/second.dart
+++ lib/screens/second/second.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class Second extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second"),
-      ),
-      body: Center(
-        child: Text("second"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Second"),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              SignInButton(
+                Buttons.Google,
+                text: "Sign in with Google",
+                onPressed: () {},
+              ),
+              SignInButton(
+                Buttons.Facebook,
+                text: "Sign up with Facebook",
+                onPressed: () {},
+              ),
+              SignInButton(
+                Buttons.GitHub,
+                text: "Sign up with GitHub",
+                onPressed: () {},
+              ),
+              SignInButton(
+                Buttons.LinkedIn,
+                text: "Sign up with LinkedIn",
+                onPressed: () {},
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SignInButton(
+                    Buttons.Google,
+                    mini: true,
+                    onPressed: () {},
+                  ),
+                  SignInButton(
+                    Buttons.Facebook,
+                    mini: true,
+                    onPressed: () {},
+                  ),
+                  SignInButton(
+                    Buttons.GitHub,
+                    mini: true,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              SignInButton(
+                Buttons.LinkedIn,
+                text: "popuntil",
+                onPressed: () {
+                  Navigator.popUntil(
+                      context, ModalRoute.withName(Navigator.defaultRouteName));
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
